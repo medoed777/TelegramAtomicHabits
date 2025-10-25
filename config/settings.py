19a7950bf66d4fd6e13522abdde6cfba20513562
Alexey <mister.skorpion4ik@mail.ru>
2025-10-25 21:45:20 +0500

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -153,8 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR, "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR, "staticfiles"]
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
